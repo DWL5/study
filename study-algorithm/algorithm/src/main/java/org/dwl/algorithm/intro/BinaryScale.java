0d31620b5f8f3311c0a18a561a388bc177bf0c5b
dwl5 <ssop6403@gmail.com>
2025-02-03 00:16:23 +0900

    add BinaryScale

---
+++ study-algorithm/algorithm/src/main/java/org/dwl/algorithm/intro/BinaryScale.java
@@ -0,0 +1,23 @@
+package org.dwl.algorithm.intro;
+
+import java.util.Scanner;
+
+public class BinaryScale {
+
+    public String solve(int decimal) {
+        StringBuilder result = new StringBuilder();
+        while (decimal > 0) {
+            result.insert(0, decimal % 2);
+            decimal /= 2;
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        BinaryScale binaryScale = new BinaryScale();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(binaryScale.solve(Integer.parseInt(str)));
+    }
+}
