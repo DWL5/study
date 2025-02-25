e6e6d4171bb03a63854e58e45f263c0c25681463
dwl5 <ssop6403@gmail.com>
2025-02-25 23:00:54 +0900

    add ShortestCharacterDistance

---
+++ study-algorithm/algorithm/src/main/java/org/dwl/algorithm/intro/ShortestCharacterDistance.java
@@ -0,0 +1,41 @@
+package org.dwl.algorithm.intro;
+
+import java.util.Scanner;
+
+public class ShortestCharacterDistance {
+    // str1 : teachermode str2 : e
+    public int[] solve(String str1, char c) {
+        int[] answer = new int[str1.length()];
+        int p = 1000;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == c) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            if (str1.charAt(i) == c) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(p, answer[i]);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ShortestCharacterDistance shortestCharacterDistance = new ShortestCharacterDistance();
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.next();
+        char c = scanner.next().charAt(0);
+        for (int x : shortestCharacterDistance.solve(str1, c)) {
+            System.out.print(x + " ");
+        }
+    }
+}
